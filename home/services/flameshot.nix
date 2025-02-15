2603d1ee4eeb57d6b916c944e42d2fb5cbfb6aea
LUVROK <pawel.2020.navtop@gmail.com>
2025-02-15 22:58:21 +0300

    feat: flameshot fix (start before tray.target)

--- home/services/flameshot.nix
+++ home/services/flameshot.nix
@@ -7,6 +7,22 @@
 			Requires = [ "graphical-session-pre.target" ];
 		};
 	};
+  
+  systemd.user.services.flameshot = {
+    Unit = {
+      Description = "Flameshot screenshot tool";
+      Requires = [ "tray.target" ];
+      After = [ "graphical-session-pre.target" "tray.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Install = { WantedBy = [ "graphical-session.target" ]; };
+
+    Service = {
+      ExecStart = "${pkgs.flameshot}/bin/flameshot";
+      Restart = "on-abort";
+    };
+  };
 
   services.flameshot = {
     enable = true;
