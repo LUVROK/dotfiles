b5854ab1fc323bf7d8e2a83243f7f928b0947bca
LUVROK <pawel.2020.navtop@gmail.com>
2025-02-15 23:58:52 +0300

    feat: flameshot fix (start before tray.target)

--- home/home.nix
+++ home/home.nix
@@ -1,5 +1,9 @@
 { config, pkgs, lib, ... }:
 
+let
+  user = "dash";  # Замените на ваш юзер
+  homeDir = "/home/${user}";
+in
 {
   programs.home-manager.enable = true;
 
@@ -36,23 +40,65 @@
   #   ];
   # };
 
-  home.file.".local/bin".source = pkgs.runCommand "merge-folders" {} ''
-    mkdir -p $out/sh-dwmblocks
-    mkdir -p $out/sh-rofi
-    mkdir -p $out/sh-others
-    mkdir -p $out/sh-nixos
-    cp -r ${./programs/dwmblocks-async/result/bin}/* $out/sh-dwmblocks
-    cp -r ${./sh/sh-dwmblocks}/* $out/sh-dwmblocks
-    cp -r ${./sh/sh-rofi}/* $out/sh-rofi
-    cp -r ${./sh/sh-others}/* $out/sh-others
-    cp -r ${./sh/sh-nixos}/* $out/sh-nixos
-    mv $out/sh-dwmblocks/dwmblocks $out/sh-dwmblocks/dwm-dwmblocks
-  '';
+  # home.file.".local/bin".source = pkgs.runCommand "merge-folders" {} ''
+  #   mkdir -p $out/sh-dwmblocks
+  #   mkdir -p $out/sh-rofi
+  #   mkdir -p $out/sh-others
+  #   mkdir -p $out/sh-nixos
+  #   cp -r ${./programs/dwmblocks-async/result/bin}/* $out/sh-dwmblocks
+  #   cp -r ${./sh/sh-dwmblocks}/* $out/sh-dwmblocks
+  #   cp -r ${./sh/sh-rofi}/* $out/sh-rofi
+  #   cp -r ${./sh/sh-others}/* $out/sh-others
+  #   cp -r ${./sh/sh-nixos}/* $out/sh-nixos
+  #   mv $out/sh-dwmblocks/dwmblocks $out/sh-dwmblocks/dwm-dwmblocks
+  # '';
 
-  home.file.".local/media".source = pkgs.runCommand "merge-folders" {} ''
-    mkdir -p $out/media
-    cp -r ${./media}/* $out
-  '';
+  # home.file.".local/media".source = pkgs.runCommand "merge-folders" {} ''
+  #   mkdir -p $out/media
+  #   cp -r ${./media}/* $out
+  # '';
+
+  systemd.user.services.sync-local-files = {    
+    Unit = {
+      After = [ "default.target" ];
+    };
+
+    Service = {
+      Type = "oneshot";
+       ExecStart = pkgs.writeShellScript "sync-local-files" ''
+        # mkdir -p ${homeDir}/.local/bin/sh-dwmblocks
+        # mkdir -p ${homeDir}/.local/bin/sh-rofi
+        # mkdir -p ${homeDir}/.local/bin/sh-others
+        # mkdir -p ${homeDir}/.local/bin/sh-nixos
+        # mkdir -p ${homeDir}/.local/media
+
+        # # Даем все права текущему пользователю
+        # chmod -R u+rwX ${homeDir}/.local/bin/sh-dwmblocks
+        # chown -R $(whoami):$(whoami) ${homeDir}/.local/bin/sh-dwmblocks
+
+        # # Копируем файлы в .local/bin
+        # rsync -a ${./sh/sh-dwmblocks}/ ${homeDir}/.local/bin/sh-dwmblocks/
+        # rsync -a ${./sh/sh-rofi}/ ${homeDir}/.local/bin/sh-rofi/
+        # rsync -a ${./sh/sh-others}/ ${homeDir}/.local/bin/sh-others/
+        # rsync -a ${./sh/sh-nixos}/ ${homeDir}/.local/bin/sh-nixos/
+        # # cp ${./programs/dwmblocks-async/result/bin/dwmblocks} ${homeDir}/.local/bin/sh-dwmblocks/dwm-dwmblocks
+
+        # # Копируем файлы в .local/media
+        # rsync -a ${./media}/ ${homeDir}/.local/media/
+      '';
+      Environment = [
+        "PATH=/home/dash/.local/bin/sh-others:${lib.makeBinPath [
+          pkgs.bash 
+          pkgs.coreutils 
+          pkgs.rsync 
+        ]}"
+      ];
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
   
   home.file.".gnupg/gpg-agent.conf" = {
     text = ''
