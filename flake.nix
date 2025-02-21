f89389878ffa078fa1350597883ec24bb6171875
LUVROK <pawel.2020.navtop@gmail.com>
2025-02-21 16:37:00 +0300

    feat: clean inputs in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,14 +4,9 @@
   inputs = {
     nixpkgs-stable.url = github:NixOS/nixpkgs/nixos-24.11;
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager.url = github:nix-community/home-manager;
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = github:nix-community/home-manager;
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nur.url = "github:nix-community/NUR";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, home-manager, nur, ... }@inputs: let
