1b54e51fb919571526a7fe3acc7c4bd8e4990c5c
LUVROK <pawel.2020.navtop@gmail.com>
2025-08-20 21:05:13 +0300

    feat(vps): add xray

--- hosts/sirius/sirius.nix
+++ hosts/sirius/sirius.nix
@@ -1,7 +1,7 @@
 { modulesPath, lib, pkgs, ... }:
 
 # nix run --no-write-lock-file github:nix-community/nixos-anywhere -- --flake .#sirius root@45.137.99.130
-# sudo nixos-rebuild switch --flake .#sirius --target-host root@45.137.99.130 --use-remote-sudo
+# nixos-rebuild switch --flake .#sirius --target-host root@45.137.99.130 --use-remote-sudo
 
 {
   system.stateVersion = "24.11";
@@ -68,5 +68,10 @@
     chown -R root:root /root/nixos-config
   '';
 
-  environment.systemPackages = with pkgs; [ vim htop curl wget ];
+  services.xray = {
+    enable = true;
+    settingsFile = ./xray.json; 
+  };
+
+  environment.systemPackages = with pkgs; [ vim htop curl wget git btop ];
 }
