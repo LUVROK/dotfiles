125fccb50d1ab756418cf3fa896ac8e2606383c5
LUVROK <pawel.2020.navtop@gmail.com>
2025-08-28 23:16:29 +0300

    build(dwmblocks): use mkDerivation with GitHub source
    Remove vendored dwmblocks code from the repo.
    
    Inject local config.h during build.
    
    Less code and simpler updates.

--- home/dwmblocks/dwmblocks-async/default.nix
+++ home/dwmblocks/dwmblocks-async/default.nix
@@ -1,11 +1,21 @@
 { pkgs }:
-# let
-#   pkgs = import <nixpkgs> {};
-# in
+
 pkgs.stdenv.mkDerivation {
   pname = "dwmblocks";
   version = "4.20.24";
-  src = ./dwmblocks;
+
+  src = pkgs.fetchFromGitHub {
+    owner = "UtkarshVerma";        # укажи владельца
+    repo  = "dwmblocks-async";         # и репозиторий
+    rev   = "fe538a7a2fc52e991a553bd76719735141658844";# pin на коммит/тег
+    hash  = "sha256-rFzgCU2wv5w7dc1xVZwX6dCFOAqM1DVK/aoFh1mwDSs="; # заполни хэш
+  };
+
+  postPatch = ''
+    cp -f ${./config.h} config.h
+  '';
+
+  # src = ./dwmblocks;
   strictDeps = true;
   buildInputs = [ pkgs.xorg.libX11 ] ++ builtins.attrValues { inherit (pkgs.xorg) libxcb xcbutil; };
   nativeBuildInputs = [ pkgs.pkg-config ];
