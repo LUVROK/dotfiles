4f0e8707986a796646864e7bc7b82b4d73462161
LUVROK <pawel.2020.navtop@gmail.com>
2025-09-02 17:43:44 +0300

    fix(fonts): adjust fonts on barnard lowdpi

--- system/dwm-flexipatch/lowdpi.h
+++ system/dwm-flexipatch/lowdpi.h
@@ -1,5 +1,5 @@
-static const char dmenufont[]            = "JetBrainsMonoNL NFP:size=18";
+static const char dmenufont[]            = "JetBrainsMonoNL NFP:size=16";
 static const char *fonts[]               = { 
-  "JetBrainsMonoNL NFP:size=18", 
-  "Font Awesome 6 Free Solid:size=18"
+  "JetBrainsMonoNL NFP:size=16", 
+  "Font Awesome 6 Free Solid:size=16"
 };
