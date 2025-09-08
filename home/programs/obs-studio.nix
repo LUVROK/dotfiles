06302b57f6e323a5fc8bba6fdeab81c2400d8717
LUVROK <pawel.2020.navtop@gmail.com>
2025-09-08 20:04:52 +0300

    feat(obs-studio): add separate config file
    remove multiple extensions

---
+++ home/programs/obs-studio.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  obs-studio = {
+    enable = true;
+
+    package = pkgs.obs-studio.override {
+      cudaSupport = true;
+    };
+
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-pipewire-audio-capture
+      obs-vkcapture
+      obs-vaapi
+    ];
+  };
+}
